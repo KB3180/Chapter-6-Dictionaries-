5aeb4fcc66b179d3d8712a8387e82306817d6c12
KB3180 <57916885+KB3180@users.noreply.github.com>
2020-02-05 12:50:57 -0600

    Chapter 6
    Final submission for Chapter 6. I did Try It 6-9

---
+++ Chapter 6 Try It 6-9 .py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+# 6-9 
+
+favorite_places = {
+    'Ezra': 'Japan', 
+    'Dray': 'Iowa',
+    'Riley': 'Japan',
+} 
+
+print ('Ezra', 'Dray', 'Riley') 
+print (favorite_places) 
+
